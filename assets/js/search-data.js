a96b7d519fc9fb631957ad275a1cf0f1dd9433cd
guptasameer112 <37142500+guptasameer112@users.noreply.github.com>
2025-01-03 16:37:15 +0000

    Deploying to gh-pages from @ guptasameer112/guptasameer112.github.io@79ec0537ad9bc60047684e01fbca91c065d5c3f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,7 +443,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%75%70%74%61%73%61%6D%65%65%72%31%31%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/https://github.com/guptasameer112", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -451,6 +458,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/sameer-gupta-198b11160/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -465,13 +479,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
