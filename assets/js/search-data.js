0007f5df2c1a6d823987d4b862025afaa5799c64
guptasameer112 <37142500+guptasameer112@users.noreply.github.com>
2025-04-19 09:19:55 +0000

    Deploying to gh-pages from @ guptasameer112/guptasameer112.github.io@aa0fd93d55513bd34f6b77ccbeee97143a970be0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-maxim-metrics-for-evaluating-agentic-workflows",
+        },{id: "post-maxim-cotools",
+      
+        title: 'Maxim: CoTools <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "Future of LLM Tool Use for Complex Reasoning.",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://www.getmaxim.ai/blog/chain-of-tools-llm-framework/", "_blank");
+        
+      },
+    },{id: "post-maxim-metrics-for-evaluating-agentic-workflows",
       
         title: 'Maxim: Metrics for Evaluating Agentic Workflows <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
